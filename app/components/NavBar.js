2177f83d2e73454857ec8397a67b826e205290a7
Tess Huang <tesshuangxj@yahoo.com>
2020-03-18 10:22:08 -0700

    Added updateLanguage func

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 export default class NavBar extends React.Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    console.log(selectedLanguage);
+    this.setState({
+      selectedLanguage
+    })
+  }
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
@@ -9,7 +23,12 @@ export default class NavBar extends React.Component {
       <ul className='flex-center'>
         {languages.map((language, index) => (
           <li key={index}>
-            <button className='nav-link btn-clear'>{language}</button>
+            <button 
+              style={language===this.state.selectedLanguage ? {color: 'red'} : null}
+              className='nav-link btn-clear' 
+              onClick={() => this.updateLanguage(language)}>
+                {language}
+            </button>
           </li>
         ))}
       </ul>
