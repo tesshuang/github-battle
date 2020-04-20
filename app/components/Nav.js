3945f5299690184e97c502b2c970f67b43dec41d
Tess Huang <tesshuangxj@yahoo.com>
2020-04-20 13:28:08 -0700

    Refactored context to useContex

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,33 +1,29 @@
 import React from 'react'
-import { ThemeConsumer } from '../context/theme'
+import ThemeContext from '../context/theme'
 import { NavLink } from 'react-router-dom'
 
 export default function Nav () {
+    const {theme, toggleTheme} = React.useContext(ThemeContext)
+    
     const activeStyle = {
       color: 'red'
     }
     return (
-      <ThemeConsumer>
-        {({ theme, toggleTheme }) => {
-          // console.log(toggleTheme)
-          return(
-            <nav className='row space-between'>
-              <ul className='row nav'>
-                <li>
-                  <NavLink activeStyle={activeStyle} exact to='/' className='nav-link'>Project</NavLink>
-                </li>
-                <li>
-                  <NavLink activeStyle={activeStyle} to='/battle' className='nav-link'>Battle</NavLink>
-                </li>
-              </ul>
-              <button 
-                style={{fontSize: 30}}
-                className='btn-clear'
-                onClick={toggleTheme}>
-                  {theme === 'light' ? '🔦' : '💡'}
-              </button>
-            </nav>
-          )}}
-      </ThemeConsumer>
+      <nav className='row space-between'>
+        <ul className='row nav'>
+          <li>
+            <NavLink activeStyle={activeStyle} exact to='/' className='nav-link'>Project</NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={activeStyle} to='/battle' className='nav-link'>Battle</NavLink>
+          </li>
+        </ul>
+        <button 
+          style={{fontSize: 30}}
+          className='btn-clear'
+          onClick={toggleTheme}>
+            {theme === 'light' ? '🔦' : '💡'}
+        </button>
+      </nav>
     )
   }
